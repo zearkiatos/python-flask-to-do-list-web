24942b690ba70b12c48dd8e52094940ea1a4d63d
zearkiatos <caprilespe@outlook.com>
2023-07-23 09:07:21 -0400

    Signup

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, session, redirect, flash, url_for
+from werkzeug.security import generate_password_hash
 from . import auth
 from flask_login import login_user, login_required, logout_user
 from app.forms.LoginForm import LoginForm
-from app.firestore_service import get_users, get_todos, get_user
+from app.firestore_service import get_users, get_todos, get_user, get_user_by_username, post_user
 from app.models.user_data import UserData
+from app.models.new_user_data import NewUserData
 from app.models.user import User
 
 INTERNAL_SERVER_ERROR = 500
@@ -22,32 +24,62 @@ def login():
         users = get_users()
         login_success = False
         for user in users:
-            login_success = user.to_dict()['password'] == password and user.to_dict()['name'] == username
+            login_success = user.to_dict()['password'] == password and user.to_dict()[
+                'name'] == username
             if login_success:
-                user_data = UserData(user.id, username=user.to_dict()['name'], password=user.to_dict()['password'])
+                user_data = UserData(user.id, username=user.to_dict()[
+                                     'name'], password=user.to_dict()['password'])
                 userFound = User(user_data)
                 print(userFound)
                 login_user(userFound)
                 break
-            # todos = get_todos(user.id)
         if login_success:
             flash('Welcome again')
             return redirect(url_for('home'))
         else:
             flash('The user does not exist')
-        # user_context = {
-        #     'user': userDict,
-        #     'todos': todos,
-        #     'username': username
-        # }
 
     return render_template('login.html', **context)
+
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash("Bye, bye, see you later")
-    
+
     return redirect(url_for('auth.login'))
 
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user_by_username(username)
+
+        if not user_doc:
+            password_hash = generate_password_hash(password)
+            new_user_data = NewUserData(username, password=password_hash)
+            post_user(new_user_data)
+
+            new_user = get_user_by_username(new_user_data.username)[0]
+
+            login_user(UserData(
+                id=new_user.id,
+                username=new_user_data.username,
+                password=new_user_data.password
+            ))
+
+            flash('Welcome!')
+
+            return redirect(url_for('home'))
+    else:
+      flash('The user exists')
+
+    return render_template('signup.html', **context)
--- app/firestore_service.py
+++ app/firestore_service.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 from app.utils.path_resolver import get_project_root
 
 credential = credentials.Certificate(str(get_project_root())+'/cert/cert.json')
@@ -19,3 +20,12 @@ def get_todos(user_id):
 
 def get_user(user_id):
     return db.collection('users').document(user_id).get()
+
+def get_user_by_username(username):
+    return db.collection('users').where(filter=FieldFilter("name", "==", username)).get()
+
+def post_user(new_user_data):
+    db.collection('users').add({
+        "name": new_user_data.username,
+        "password": new_user_data.password
+    })
